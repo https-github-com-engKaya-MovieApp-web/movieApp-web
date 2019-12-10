1ef33f7cec161dd520a47e9cc3ac0e72a6bae6ea
unknown <ibrahimsabitkaya@gmail.com>
2019-12-10 16:08:45 +0300

    Movie Class ı ve Repos'u oluşturuldu

---
+++ movieApp-web/_movieApp/_movieApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _movieApp.WebUI.Models;
+
+namespace _movieApp.WebUI.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            //    MovieCategoryModel model = new MovieCategoryModel();
+            //    model.Categories = CategoryRep.Categories;
+            //    model.Movies = Repository.Movies;
+
+            return View(Repository.Movies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            //MovieCategoryModel model = new MovieCategoryModel();
+            //model.Categories = CategoryRep.Categories;
+            //model.Movie = Repository.GetByID(id);
+
+            return View(Repository.GetByID(id));
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ movieApp-web/_movieApp/_movieApp.WebUI/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _movieApp.WebUI.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime LoadTime { get; set; }
+    }
+}
